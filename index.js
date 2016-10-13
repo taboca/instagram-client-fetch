fecdb2537c5f960556c4964f6c0c331f2913164d
Marcio <taboca@gmail.com>
2016-10-13 17:54:14 -0300

    feed

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var express = require('express');
 var config = require('./config.js');
 var api = require('instagram-node').instagram();
 var app = express();
- 
+
 api.use({
   client_id: config.instagram.id,
   client_secret: config.instagram.secret
 });
 
 var redirect_uri = 'http://telasocial.com:3333/handleauth';
- 
+
 exports.authorize_user = function(req, res) {
   res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes','public_content'], state: 'a state' }));
 };
- 
+
 exports.handleauth = function(req, res) {
   api.authorize_user(req.query.code, redirect_uri, function(err, result) {
     if (err) {
@@ -26,12 +26,26 @@ exports.handleauth = function(req, res) {
     }
   });
 };
- 
-// This is where you would initially send users to authorize 
+
+exports.listMedia = function(req, res) {
+
+  api.use({ access_token: config.instagram.token } );
+
+  api.tag_media_recent('latinoware', function(err, medias, pagination, remaining, limit) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify());
+  });
+
+};
+
+
+// This is where you would initially send users to authorize
 app.get('/authorize_user', exports.authorize_user);
-// This is your redirect URI 
+// This is your redirect URI
 app.get('/handleauth', exports.handleauth);
- 
+
+app.get('/feed', exports.listMedia);
+
 http.createServer(app).listen(3333, function(){
   console.log("Express server listening on port " + app.get('port'));
 });
--- search.js
+++ search.js
@@ -8,11 +8,9 @@ api.tag_search('latinoware', function(err, result, remaining, limit) {
   console.log(JSON.stringify(result));
 });
 
-
-//api.tag_media_recent('latinoware', [options,] function(err, medias, pagination, remaining, limit) {});
+// api.tag_media_recent('latinoware', [options,] function(err, medias, pagination, remaining, limit) {});
 
 // Note from instagram API As another example, let's consider an endpoint that returns a list of media: /tags/{tag-name}/media/recent. The response returned by this endpoint will contain only media with the given tag, as expected. But instead of returning media from any public Instagram user, it will return only media that belongs to your sandbox users, restricted to the last 20 for each user.
-
 
 api.tag_media_recent('latinoware', function(err, medias, pagination, remaining, limit) {
   console.log(JSON.stringify(medias));
@@ -28,5 +26,4 @@ function fetchMedia(id) {
         console.log('media url '+media.images.standard_resolution.url);
    });
 }
-
 
